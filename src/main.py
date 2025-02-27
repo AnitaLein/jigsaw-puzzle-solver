8947d6d6030148213f529339213db8cb5d074b9d
Daniel Eisele <daniel_eisele@gmx.de>
2025-02-28 00:10:16 +0100

    Process puzzle eda in main.py

--- src/main.py
+++ src/main.py
@@ -1,39 +1,25 @@
-import cv2 as cv
-from Puzzle import *
-from arrange_pieces import *
-from contours import *
-from corners import *
-from intersection import *
-from piece_classification import *
-from edge_matching import *
+from extract_pieces import main as extract_pieces_main
+from classify_piece import main as classify_piece_main
+from compute_similarities import main as compute_similarities_main
 
+data_dir = "../data"
+work_dir = "../work"
+puzzle_name = "eda"
+scans = ["1", "2"]
 
-img = cv.imread('../data/misc/eda_black_merged.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)     
-assert img is not None, "file could not be read, check with os.path.exists()"
+for scan in scans:
+    extract_pieces_main(data_dir, puzzle_name, scan, work_dir)
 
-all_output_folders = [
-    'output_rotated_contours', 'translated_contours', 'output_intersection', 
-    'output_intersection_mask', 'output_corners', 'output_edges_contours', 
-    'output_edges', 'output_split_edges', 'output_intersection_drawing',
-    'output_created_piece']
-for folder in all_output_folders:
-    files = glob.glob(os.path.join(folder, '*.png'))
-    for f in files:
-        os.remove(f)
+    for piece_number in range(0, 6):
+        classify_piece_main(puzzle_name, scan + "_" + str(piece_number), work_dir)
 
-bilateral_blur = cv.bilateralFilter(img, 9, 75, 75)
+compute_similarities_main(puzzle_name, work_dir)
 
-#piece_contour = find_contours(bilateral_blur)
-
-puzzle_pieces = classify_piece(img, gray)
-grid = [[None for _ in range(len(puzzle_pieces))] for _ in range(len(puzzle_pieces))]
-similarity_matrix= compute_similarity_matrix(puzzle_pieces)
-corner_pieces = load_puzzle_pieces_from_csv('corner_pieces')
-#grid = solvePuzzle(puzzle_pieces, similarity_matrix, corner_pieces)
-grid, connected_pieces, rotated_pieces = place_corner_piece(grid, corner_pieces, puzzle_pieces)
-grid =place_edge_pieces(grid, puzzle_pieces, similarity_matrix, connected_pieces, rotated_pieces)
-save_grid_to_csv(grid)
+#corner_pieces = load_puzzle_pieces_from_csv('corner_pieces')
+##grid = solvePuzzle(puzzle_pieces, similarity_matrix, corner_pieces)
+#grid, connected_pieces, rotated_pieces = place_corner_piece(grid, corner_pieces, puzzle_pieces)
+#grid = place_edge_pieces(grid, puzzle_pieces, similarity_matrix, connected_pieces, rotated_pieces)
+#save_grid_to_csv(grid)
 
 print('done')
 
